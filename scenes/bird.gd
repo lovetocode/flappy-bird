6559afa56de0e6d14e6267c025a4a75ab72372a8
Daniel Thornton <dshaggy28@yahoo.com>
2023-10-12 17:34:55 -0700

    Worked on the game

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -1,9 +1,17 @@
 extends CharacterBody2D
 
+class_name Bird
+
 @export var gravity = 900.0
 @export var jump_force = 200
-@export var rotation_speed = 20
+@export var rotation_speed = 2
 
 @onready var animation_player = $AnimationPlayer
 
 var max_speed = 200
+
+func _ready():
+	velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	pass
