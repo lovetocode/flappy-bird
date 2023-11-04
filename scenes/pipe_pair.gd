f806878545022eccfed4273fb522b20c3db136b7
Daniel Thornton <dshaggy28@yahoo.com>
2023-11-03 17:01:42 -0700

    finished pipe pair scene.

---
+++ scenes/pipe_pair.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+class_name  PipePair
+
+signal bird_enterd
+signal point_scored
+
+var speed = 0
+
+func set_speed(new_speed):
+	speed = new_speed
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x = speed * delta
+	
+func _on_body_entered(body):
+	bird_enterd.emit()
+	
+func _on_point_scored(body):
+	point_scored.emit()
+	
